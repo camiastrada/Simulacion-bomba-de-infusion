44209ebef5ad338d3a7e4b54749b93f8c5da74ca
camiastrada <camiastradaw@gmail.com>
2026-06-20 10:26:23 -0300

    arreglar comentarios

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from simulacion import Simulacion
 
 if __name__ == "__main__":
 
-    # simular = Simulacion("SimulacionBombaInfusion")
-    # simular.iniciar_simulacion(3600.0,None,None,None,None)
-    # simular.mostrar_metricas()
-    # simular.graficar_timeline()
-    # simular.graficar_caudal("Escenario1_Normal") 
-    # simular.graficar_estado_bomba("Escenario1_Normal")
-    # simular.contar_detenciones_preventivas()
+    simular = Simulacion("SimulacionBombaInfusion")
+    simular.iniciar_simulacion(3600.0,None,None,None,None)
+    simular.mostrar_metricas()
+    simular.graficar_timeline()
+    simular.graficar_caudal("Escenario1_Normal") 
+    simular.graficar_estado_bomba("Escenario1_Normal")
+    simular.contar_detenciones_preventivas()
 
 
     # #Escenario 1: Funcionamiento normal sin fallas
@@ -24,21 +24,21 @@ if __name__ == "__main__":
 
     # #Escenario 2: Cambia orden medica durante la infusion
     # #el generador emite una orden de 50, y a los 2 segundo emite orden de 80... luego no deberia emitir nada por 30000 segundos
-    # valores_f = iter([50.0, 80.0])
-    # def f():
-    #      return next(valores_f, 80.0)  # después sigue devolviendo 80
+    valores_f = iter([50.0, 80.0])
+    def f():
+         return next(valores_f, 80.0)  # después sigue devolviendo 80
 
-    # valores_g = iter([1.0, 2.0, 30000.0])
-    # def g():
-    #     return next(valores_g, 3000)
-    # simular = Simulacion("SimulacionBombaInfusion")
-    # simular.iniciar_simulacion(36000.0,f,g,None,None)
-    # simular.mostrar_metricas()
+    valores_g = iter([1.0, 2.0, 30000.0])
+    def g():
+        return next(valores_g, 3000)
+    simular = Simulacion("SimulacionBombaInfusion")
+    simular.iniciar_simulacion(36000.0,f,g,None,None)
+    simular.mostrar_metricas()
 
 
     # Escenario 6: Fin de bolsa con confirmación del enfermero
     # Se fija caudal alto para vaciar la bolsa en un tiempo corto de simulación.
-    # El enfermero confirma la alarma baja luego de 2s.
+    # El enfermero confirma la alarma baja luego de 20s.
     def funcion_caudal_max():
          return 200.0
 
